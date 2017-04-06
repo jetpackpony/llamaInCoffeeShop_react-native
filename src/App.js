d12715ea9c947f9d38c4e9747e1e20e9e4d7ffe0
jetpackpony <jetpackpony@gmail.com>
2017-04-06 20:08:58 +0300

    Add TICK action, RAF and frame rate counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  Text,
   View
 } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+
 import Reducer from './reducer/Reducer';
 import Player from './Player';
-import { jumpStart } from './actions/Actions';
+import { jumpStart, tick } from './actions/Actions';
 
 const store = createStore(
   Reducer,
   applyMiddleware(logger)
 );
 
+const lastFrameCount = 0;
+const frameCounter = 0;
+const frameRate = 0;
+
 export default class LlamaInCoffeeShop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...store.getState(), frameRate: 0 };
+    this.updateFrame = this.updateFrame.bind(this);
+    this.timer = null;
+  }
+
+  updateFrame(timestamp) {
+    frameCounter++;
+    if (timestamp - lastFrameCount >= 1000) {
+      frameRate = frameCounter;
+      frameCounter = 0;
+      lastFrameCount = timestamp;
+    }
+
+    // Dispatch an action to update all the positions
+    store.dispatch(tick(timestamp));
+
+    // Update application
+    this.setState({
+      ...store.getState(),
+      frameRate
+    });
+
+    // Request next frame
+    this.timer = requestAnimationFrame(this.updateFrame);
+  }
+
+  componentWillMount() {
+    this.timer = requestAnimationFrame(this.updateFrame);
+  }
+
+  componentWillUnmount() {
+    cancelAnimationFrame(this.timer);
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <Text>{this.state.frameRate}</Text>
         <Player
-          position={store.getState().player.coord}
+          position={this.state.player.coord}
           onPressIn={() => { store.dispatch(jumpStart()); }}
         />
       </View>
@@ -30,7 +73,7 @@ export default class LlamaInCoffeeShop extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     padding: 100
   }
--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,20 +1,54 @@
 import { ActionTypes } from '../actions/Actions';
+const { JUMP_START, TICK } = ActionTypes;
 
 const initialState = {
   player: {
     coord: 10,
-    inJump: false
+    inJump: false,
+    lastUpdate: 0
   }
+};
+
+const speed = 0.05; // pixels per milisecond
+const performTick = (state, timestamp) => {
+  // If the first frame, just set the new timestamp
+  if (state.lastUpdate === 0) {
+    return { ...state, lastUpdate: timestamp };
+  }
+
+  // Calculate number of pixels to move
+  const frameLag = timestamp - state.lastUpdate;
+  const pixToMove = frameLag * speed;
+
+  // If less than one pixel, skip this frame
+  if (pixToMove < 1) {
+    return state;
+  }
+
+  // Else, update coordinates and timestamp
+  return {
+    ...state,
+    coord: state.coord + pixToMove,
+    lastUpdate: timestamp
+  };
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.JUMP_START:
+    case JUMP_START:
       return {
         ...state,
         player: {
           ...state.player,
           inJump: true
+        }
+      };
+    case TICK:
+      return {
+        ...state,
+        player: {
+          ...state.player,
+          ...performTick(state.player, action.payload.timestamp)
         }
       };
     default:
